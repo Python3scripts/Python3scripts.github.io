39db417ae6965e2f979fa81742be09cfbb7819d5
Python3scripts <46499762+Python3scripts@users.noreply.github.com>
2019-01-08 20:35:33 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function gi(...args){
+	return document.getElementById().style;
+}
+function chalon(){
+	gi("chalbox").display = "block";
+	gi("chal").backgroundColor = "#00BFFF";
+	gi("on").backgroundColor = "white";
+}
+function chalout(){
+	gi("chalbox").display = null;
+	gi("chal").backgroundColor = null;
+}
